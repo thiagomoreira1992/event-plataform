0b26ed45f75b5686f63238bd6b1dea1fc78ea8c3
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2022-06-22 20:47:12 -0300

    Criado componente de video e criado as rotas e a integração com o GraphCMS para retornar dados dos videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { ApolloProvider } from "@apollo/client";
 import { Event } from "./Pages/Event";
+import { Router } from "./Router";
+
+import { client } from "./lib/apollo";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
