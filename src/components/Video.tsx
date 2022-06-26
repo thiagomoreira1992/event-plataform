1adf13085888711c7a8d5181d40410ac2ff2ae53
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2022-06-26 15:41:04 -0300

    Finalizada aula 5 usando o graphql-codegen

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,79 +1,65 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
-import '@vime/core/themes/default.css';
-import { gql, useQuery } from "@apollo/client";
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Image,
+  Lightning,
+} from "phosphor-react";
+import "@vime/core/themes/default.css";
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
-
-const GET_LESSON_BY_SLUG = gql`
-    query GetLessonBySlug($slug: String) {
-  lesson(where: {slug: $slug}) {
-    title
-    videoId
-    description
-    teacher {
-      bio
-      avatarURL
-      name
-    }
-  }
-}
-`;
-
-interface GetLessonBySLugResponse{
-    lesson: {
-        title: string;
-        videoId: string;
-        description: string;
-        teacher: {
-            bio: string;
-            avatarURL: string;
-            name: string;
-        }
-    }
-}
 interface VideoProps {
-    lessonSlug: string;
+  lessonSlug: string;
 }
 export function Video(props: VideoProps) {
-
-    const { data } = useQuery<GetLessonBySLugResponse>(GET_LESSON_BY_SLUG, {
-        variables: {
-            slug: props.lessonSlug
-        }
-    })
-    if (!data){
-        return <div className="flex-1"><p>Carregando</p></div>
-    }
+  const { data } = useGetLessonBySlugQuery({
+    variables: {
+      slug: props.lessonSlug,
+    },
+  });
+  if (!data || !data.lesson) {
+    return (
+      <div className="flex-1">
+        <p>Carregando</p>
+      </div>
+    );
+  }
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-            <Player>
-                <Youtube videoId={data.lesson.videoId}/>
-                <DefaultUi />
-            </Player>
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
         </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
-              {data.lesson.title}
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
               {data.lesson.description}
             </p>
-            <div className="flex items-center gap-4 mt-6">
-                <img 
-                className="h-16 2 -16 rounded-full border-2 border-blue-500 "
-                src={data.lesson.teacher.avatarURL} 
-                alt="" />
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 2 -16 rounded-full border-2 border-blue-500 "
+                  src={data.lesson.teacher.avatarURL}
+                  alt=""
+                />
                 <div className="leading-relaxed">
-                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                    <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                  <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data.lesson.teacher.bio}
+                  </span>
                 </div>
-            </div>
+              </div>
+            )}
           </div>
           <div className="flex flex-col gap-4 ">
             <a
@@ -91,39 +77,41 @@ export function Video(props: VideoProps) {
           </div>
         </div>
         <div className="gap-8 mt-20 grid grid-cols-2">
-        <a 
-        href="" 
-        className=" bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          <a
+            href=""
+            className=" bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
             <div className="bg-green-700 h-full p-6 flex items-center">
-                <FileArrowDown size={40}/>
+              <FileArrowDown size={40} />
             </div>
             <div className="py-6 leading-relaxed">
-                <strong className="text-2xl">Material Complementar</strong>
-                <p className="text-sm text-gray-200">
-                    Acesse o material complementar para acelerar seu desenvolvimento
-                </p>
+              <strong className="text-2xl">Material Complementar</strong>
+              <p className="text-sm text-gray-200">
+                Acesse o material complementar para acelerar seu desenvolvimento
+              </p>
             </div>
             <div className="h-full p-6 flex items-center">
-                <CaretRight size={24}/>
+              <CaretRight size={24} />
             </div>
-        </a>
-        <a 
-        href="" 
-        className=" bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          </a>
+          <a
+            href=""
+            className=" bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
             <div className="bg-green-700 h-full p-6 flex items-center">
-                <Image size={40}/>
+              <Image size={40} />
             </div>
             <div className="py-6 leading-relaxed">
-                <strong className="text-2xl">Wallpapers exclusivos</strong>
-                <p className="text-sm text-gray-200">
-                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
-                </p>
+              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-sm text-gray-200">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                máquina
+              </p>
             </div>
             <div className="h-full p-6 flex items-center">
-                <CaretRight size={24}/>
+              <CaretRight size={24} />
             </div>
-        </a>
-        
+          </a>
         </div>
       </div>
     </div>
